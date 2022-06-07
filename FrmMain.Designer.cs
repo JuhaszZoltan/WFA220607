86f1c5e7eb2ea144f0438acc2793e665d1030073
JuhaszZoltan <JuhaszZoltanMate@yahoo.com>
2022-06-07 13:50:10 +0200

    Add project files.

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WFA220607
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNev = new System.Windows.Forms.TextBox();
+            this.btnKoszon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(16, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Név:";
+            // 
+            // tbNev
+            // 
+            this.tbNev.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbNev.Location = new System.Drawing.Point(150, 25);
+            this.tbNev.Name = "tbNev";
+            this.tbNev.Size = new System.Drawing.Size(445, 64);
+            this.tbNev.TabIndex = 1;
+            // 
+            // btnKoszon
+            // 
+            this.btnKoszon.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnKoszon.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnKoszon.Location = new System.Drawing.Point(12, 111);
+            this.btnKoszon.Name = "btnKoszon";
+            this.btnKoszon.Size = new System.Drawing.Size(583, 79);
+            this.btnKoszon.TabIndex = 2;
+            this.btnKoszon.Text = "Köszönj!";
+            this.btnKoszon.UseVisualStyleBackColor = false;
+            this.btnKoszon.Click += new System.EventHandler(this.BtnKoszon_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 217);
+            this.Controls.Add(this.btnKoszon);
+            this.Controls.Add(this.tbNev);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmMain";
+            this.Text = "Első program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNev;
+        private System.Windows.Forms.Button btnKoszon;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA220607
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnKoszon_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbNev.Text))
+            {
+                MessageBox.Show("Előbb írd be a neved!");
+            }
+            else
+            {
+                MessageBox.Show($"Szia {tbNev.Text}!");
+            }
+        }
+    }
+}
